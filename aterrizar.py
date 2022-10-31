87d3f1ed745db710dccb4ae0230763ba092f5b60
franco@mumuki.org <franco@mumuki.org>
2022-10-31 10:43:23 -0300

    Asegurando que haya siempre un atributo comprador

--- aterrizar.py
+++ aterrizar.py
@@ -2,8 +2,14 @@ class Cliente:
     pass
 
 class Asiento:
+    def __init__(self):
+        self.comprador = None
+
     def venderse(self, cliente):
         self.comprador = cliente
 
     def vendido_a(self, cliente):
         return self.comprador == cliente
+
+    def esta_vendido(self):
+        return self.comprador is not None
--- test_aterrizar.py
+++ test_aterrizar.py
@@ -7,3 +7,12 @@ def test_a48_puede_venderse_a_ro():
     a48.venderse(ro)
 
     assert a48.vendido_a(ro)
+    assert a48.esta_vendido()
+
+
+def test_si_a48_no_se_vende_no_esta_vendido():
+    ro = Cliente()
+    a48 = Asiento()
+
+    assert not a48.vendido_a(ro)
+    assert not a48.esta_vendido()
