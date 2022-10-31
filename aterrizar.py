aa84cf592e965f459e23f2eb7752661826099aa6
franco@mumuki.org <franco@mumuki.org>
2022-10-31 11:11:48 -0300

    Implementando reserva y estados

--- aterrizar.py
+++ aterrizar.py
@@ -1,15 +1,37 @@
 class Cliente:
     pass
 
+class Libre:
+    def vender_a(self, asiento, cliente):
+        asiento.comprador = cliente
+        asiento.estado = Vendido()
+
+class Vendido:
+    def vender_a(self, asiento, cliente):
+        asiento.reservante = cliente
+        asiento.estado = Reservado()
+
+class Reservado:
+    def vender_a(self, asiento, cliente):
+        # asiento.estado = Sobrereservador()
+        pass
+
 class Asiento:
     def __init__(self):
         self.comprador = None
+        self.estado = Libre()
 
     def venderse(self, cliente):
-        self.comprador = cliente
+        self.estado.vender_a(self, cliente)
 
     def vendido_a(self, cliente):
         return self.comprador == cliente
 
     def esta_vendido(self):
         return self.comprador is not None
+
+    def reservado_a(self, cliente):
+        return self.reservante == cliente
+
+    def esta_reservado(self):
+        return self.reservante is not None
--- test_aterrizar.py
+++ test_aterrizar.py
@@ -1,9 +1,18 @@
 from aterrizar import *
 
-def test_a48_puede_venderse_a_ro():
+ro = None
+ana = None
+a48 = None
+
+def setup_function():
+    global ro
+    global ana
+    global a48
     ro = Cliente()
+    ana = Cliente()
     a48 = Asiento()
 
+def test_a48_puede_venderse_a_ro():    
     a48.venderse(ro)
 
     assert a48.vendido_a(ro)
@@ -11,8 +20,21 @@ def test_a48_puede_venderse_a_ro():
 
 
 def test_si_a48_no_se_vende_no_esta_vendido():
-    ro = Cliente()
-    a48 = Asiento()
-
     assert not a48.vendido_a(ro)
     assert not a48.esta_vendido()
+
+
+def test_si_viene_ana_y_compra_un_asiento_vendido_no_se_vende():
+    a48.venderse(ro)
+    a48.venderse(ana)
+
+    assert a48.vendido_a(ro)
+    assert not a48.vendido_a(ana)
+
+
+def test_si_viene_ana_y_compra_un_asiento_vendido_se_reserva():
+    a48.venderse(ro)
+    a48.venderse(ana)
+
+    assert a48.reservado_a(ana)
+    assert a48.esta_reservado()
